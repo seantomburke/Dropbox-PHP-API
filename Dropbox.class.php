324fc8d1018e7c855eb15176d7f12b28afe18dd2
Sean Burke <seantomburke@users.noreply.github.com>
2012-07-18 23:34:02 -0700

    Removed debugging echos

--- Dropbox.class.php
+++ Dropbox.class.php
@@ -25,38 +25,20 @@ class Dropbox
 
 	public function __construct()
 	{	
-		
-		echo 'Step 0: Dumping Session<br>';
-		var_dump($_SESSION);
-		echo'<br>';
 		$this->oauth_token_secret 	= $_SESSION['oauth_token_secret'];
 		$this->oauth_request_token 	= $_SESSION['oauth_request_token'];
 		$this->oauth_access_token 	= $_SESSION['oauth_access_token'];
 		$this->oauth_signature 		= $_SESSION['oauth_signature'];
-		$this->uid 					= $_SESSION['uid'];				
-		
-		echo '<br>Start<br>
-			APP_KEY:'.self::$APP_KEY.'<br>
-			APP_SECRET:'.self::$APP_SECRET.'<br>
-			OAuth_token_secret: '.$this->oauth_token_secret.'<br>
-			OAuth_request_token: '.$this->oauth_request_token.'<br>
-			OAuth_access_token: '.$this->oauth_access_token.'<br>
-			OAuth_signature: '.$this->oauth_signature.'<br>';
+		$this->uid 					= $_SESSION['uid'];		
 			  
-		echo 'if('.!isset($this->oauth_signature).' OR '.!isset($this->oauth_access_token).')';
 		if(!isset($this->oauth_signature) || !isset($this->oauth_access_token))
 		{
-			echo 'if('.!isset($this->oauth_token_secret).' OR '.!isset($this->oauth_request_token).')';
 			if(isset($_GET['uid']) && isset($_GET['oauth_token']) && isset($this->oauth_token_secret) && isset($this->oauth_request_token))
 			{
-				echo'<br>';echo'<br>';echo 'Step 2: Processing CallBack()';
-				echo'<br>';
 				$this->processCallBack();
 			}
 			else
 			{
-				echo'<br>';echo'<br>';echo 'Step 1: Requesting()';
-				echo'<br>';
 				$this->request();
 			}
 		}
@@ -68,19 +50,15 @@ class Dropbox
 		$ch = curl_init(); 
 		//Dropbox uses plaintext OAuth 1.0; make the header for this request
 		$headers = array('Authorization: OAuth oauth_version="1.0", oauth_signature_method="PLAINTEXT", oauth_consumer_key="'.self::$APP_KEY.'", oauth_signature="'.self::$APP_SECRET.'&"');  
-		echo'<br>Setting Request Headers<br>';var_dump($headers);echo'<br>';
 		// set cURL options and execute
 		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);  
 		curl_setopt($ch, CURLOPT_URL, "https://api.dropbox.com/1/oauth/request_token");  
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);  
 		$request_token_response = curl_exec($ch);
-		echo $request_token_response;  
 		
 		//	parse the returned data which has the format:
 		// "oauth_token=<access-token>&oauth_token_secret=<access-token-secret>"
 		parse_str($request_token_response, $parsed_request_token);
-		echo '<br>Response to Request<br>';
-		var_dump($parsed_request_token);
 		$json_access = json_decode($request_token_response);
 		if($json_access->error)
 		{
@@ -97,7 +75,6 @@ class Dropbox
 		//get the request URL; this is where you send the user to authorize your request. Be sure to set the CALLBACK_URL before doing this.
 		$this->request_token_url = 'https://www.dropbox.com/1/oauth/authorize?oauth_token='.$parsed_request_token['oauth_token'].'&oauth_callback='.self::$CALLBACK_URL;	
 		
-		echo '<br><a href="'.$this->request_token_url.'">Get Dropbox Syncing</a><br>';
 	}
 	
 	function processCallBack()
@@ -108,16 +85,12 @@ class Dropbox
 		//notice the signature is a concatenation of the app_secret and the token_secret
 		$ch = curl_init();  
 		$headers = array('Authorization: OAuth oauth_version="1.0", oauth_signature_method="PLAINTEXT", oauth_consumer_key="'.self::$APP_KEY.'", oauth_token="'.$this->oauth_request_token.'", oauth_signature="'.self::$APP_SECRET.'&'.$this->oauth_token_secret.'"');  
-		echo'<br>Setting Callback headers<br>';var_dump($headers);echo'<br>';
 		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);  
 		curl_setopt($ch, CURLOPT_URL, "https://api.dropbox.com/1/oauth/access_token");  
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);  
 		$access_token_response = curl_exec($ch);  
 		//execute and parse
-		echo $access_token_response;
 		parse_str($access_token_response, $parsed_access_token);
-		echo'<br>';echo '<br>Response to CallBack<br>';
-		var_dump($parsed_access_token);
 		$json_access = json_decode($access_token_response);
 		if($json_access->error)
 		{
@@ -135,22 +108,13 @@ class Dropbox
 		$this->oauth_signature 				= self::$APP_SECRET.'&'.$parsed_access_token['oauth_token_secret'];	
 		$this->uid		 					= $_GET['uid'];
 		
-		echo '<br>After Call Back<br>
-			APP_KEY:'.self::$APP_KEY.'<br>
-			APP_SECRET:'.self::$APP_SECRET.'<br>
-			OAuth_token_secret: '.$this->oauth_token_secret.'<br>
-			OAuth_request_token: '.$this->oauth_request_token.'<br>
-			OAuth_access_token: '.$this->oauth_access_token.'<br>
-			OAuth_signature: '.$this->oauth_signature.'<br>';
 		return true;
 	}
 	
 	function call($url)
 	{
-		echo '<br>Step 3: Calling API<br>';
 		$ch = curl_init(); 
 		$headers = array('Authorization: OAuth oauth_version="1.0", oauth_signature_method="PLAINTEXT", oauth_consumer_key="'.self::$APP_KEY.'", oauth_token="'.$this->oauth_access_token.'", oauth_signature="'.$this->oauth_signature.'"');  
-		echo '<br>';var_dump($headers);echo '<br>';
 		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);  
 		curl_setopt($ch, CURLOPT_URL, $url);  
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);  
@@ -166,7 +130,6 @@ class Dropbox
 	
 	function hasAccess()
 	{
-		echo '('.$this->oauth_access_token.' && '.$this->oauth_signature.' && '.$this->uid.')';
 		return ($this->oauth_access_token && $this->oauth_signature && $this->uid);
 	}
 	
